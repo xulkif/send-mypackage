cf127308a5ccaf9958a70ae122c98d26714c23ba
xulkif <zulkifazher2@gmail.com>
2025-10-18 20:30:30 +0300

    header

--- frontend/src/user/ux/header.jsx
+++ frontend/src/user/ux/header.jsx
@@ -4,9 +4,9 @@ import { useSelector } from "react-redux"
 const UserHeader=()=>{
     const{user}=useSelector(state=>state.user)
     return<header className="w-full py-4 flex items-center  px-2 border-1 border-gray-100">
-       <label>{user?.first_name||""}</label>
-       <label>{user?.last_name||""}</label>
-       <label>{user?.username||""}</label>
+       <label> First Name{user?.first_name||""}</label>
+       <label> Last Name{user?.last_name||""}</label>
+       <label> User Name{user?.username||""}</label>
 
 
     </header>
